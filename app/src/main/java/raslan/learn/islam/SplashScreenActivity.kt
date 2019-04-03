c52193b42fd5ab118d112e7739a0303c29adb22b
Ibrahim Ali <sun_moon545@yahoo.com>
2019-04-03 02:08:09 +0200

    Run graph-ql, basic configurations.

---
+++ app/src/main/java/raslan/learn/islam/SplashScreenActivity.kt
@@ -0,0 +1,18 @@
+package raslan.learn.islam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_splash_screen)
+    }
+
+}
